c817568e8e5de7297468a7cf284052721e57f8e8
dai <msdenisseanaya@gmail.com>
2019-04-03 17:47:04 -0700

    Finished app demo -Bitnami used

---
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    //2ru6TfFhJ6Os
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("9a9678787320f180438ee23201515eedd4d8f06b")
+            .clientKey("4a706a65a48b1b2e648b29404cec07254a780310")
+            .server("http://18.217.167.159:80/parse/")
+            .build()
+    );
+
+    //ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
